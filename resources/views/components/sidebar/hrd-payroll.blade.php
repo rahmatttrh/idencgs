44896689180a167254a4c50c3da72e9b222356e5
Rahmat Hidayat <rahmattrust@gmail.com>
2025-01-07 20:27:45 +0700

    add master data sidebar hrd payroll

--- resources/views/components/sidebar/hrd-payroll.blade.php
+++ resources/views/components/sidebar/hrd-payroll.blade.php
@@ -32,6 +32,42 @@
       <p>Report</p>
    </a>
 </li> --}}
+<li class="nav-item">
+   <a data-toggle="collapse" href="#vessel">
+      <i class="fas fa-server"></i>
+      <p>Master Data</p>
+      <span class="caret"></span>
+   </a>
+   <div class="collapse" id="vessel">
+      <ul class="nav nav-collapse">
+         <li>
+            <a href="{{route('unit')}}">
+               <span class="sub-item">Bisnis Unit</span>
+            </a>
+         </li>
+         <li>
+            <a href="{{route('department')}}">
+               <span class="sub-item">Department</span>
+            </a>
+         </li>
+         <li>
+            <a href="{{route('designation')}}">
+               <span class="sub-item">Level</span>
+            </a>
+         </li>
+         <li>
+            <a href="{{route('position')}}">
+               <span class="sub-item">Jabatan</span>
+            </a>
+         </li>
+         <li>
+            <a href="{{route('so')}}">
+               <span class="sub-item">Struktur Organisasi</span>
+            </a>
+         </li>
+      </ul>
+   </div>
+</li>
 
 <li class="nav-item">
    <a data-toggle="collapse" href="#payroll">
